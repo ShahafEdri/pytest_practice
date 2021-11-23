a041a6b10503817de7907c3216be080858aad788
Shahaf Edri <edri.sue@gmail.com>
2021-11-23 15:47:03 +0200

    first commit: ** Calculator * add of all the project files so far * added  a calculator class * added calculator methods * added main script with CLI menu to invoke calculator class methods * added calculator Exeption ** pytest * added 'add' test * added 'subtract' test * added 'divide' test * added 'multiply' test * added 'devide by zero' test * added 'string input test'

---
+++ calculator.py
@@ -0,0 +1,61 @@
+import sys
+import numbers
+
+
+class CalculatorError(Exception):
+    """doc for Calculator Error"""
+
+
+class Calculator:
+    """doc string"""
+
+    def add(self, a, b):
+        self._check_operand(a)
+        self._check_operand(b)
+        try:
+            return a + b
+        except TypeError:
+            raise CalculatorError("error massege")
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        try:
+            return a / b
+        except ZeroDivisionError:
+            raise CalculatorError("You can't devide by zero")
+
+    def _check_operand(self, operand):
+        if not isinstance(operand, numbers.Number):
+            raise CalculatorError(f'"{operand}" was not a number')
+
+
+if __name__ == '__main__':
+    print("Let's Calculate!")
+
+    calculator = Calculator()
+    operations = [
+        calculator.add,
+        calculator.subtract,
+        calculator.multiply,
+        calculator.divide,
+    ]
+
+    while True:
+        for i, operation in enumerate(operations, start=1):
+            print(f"{i}: {operation.__name__}")
+        print("q: quit")
+        operation = int(input("Pick an operation:  "))
+        if operation == 'q':
+            sys.exit()
+        op = int(i)
+        a = float(input("What is a? "))
+        b = float(input("What is b? "))
+        try:
+            print(operations[operation-1](a, b))
+        except CalculatorError as ex:
+            print(ex)
