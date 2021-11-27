f2acd4afca1fb33dfcc0afd0d95f5c88ced232ce
Shahaf Edri <edri.sue@gmail.com>
2021-11-27 13:07:28 +0200

    added mark.parametrize to all relevent tests

--- test_test-name1.py
+++ test_test-name1.py
@@ -4,66 +4,70 @@ import pytest
 
 
 @pytest.mark.basic_functionality
-@pytest.mark.parametrize("input1,input2,expected", [(3, 1, 4)])
+@pytest.mark.parametrize("input1,input2,expected", [(3, 1, 4),      # + + addition
+                                                    (-3, 1, -2),    # - + addition
+                                                    (-3, -1, -4),   # - - addition
+                                                    (3, -1, 2)])    # + - addition
 def test_add(input1, input2, expected):
-    # Arrange
     calculator = Calculator()
-    # Act
     result = calculator.add(input1, input2)
-    # Assert
     assert result == expected, "test_add_assertion_error"
 
 
 @pytest.mark.weird_functionality
-def test_add_weird_stuff():
-    # Arrange
+@pytest.mark.parametrize("input1,input2,error_type", [("three", 1, CalculatorError),
+                                                      (3, "one", CalculatorError)])
+def test_string_input_and_int_input_to_add(input1, input2, error_type):
     calculator = Calculator()
-    # Act
-    with pytest.raises(CalculatorError) as context:
-        result = calculator.add("three", 1)
+    with pytest.raises(error_type) as context:
+        result = calculator.add(input1, input2)
 
 
 @pytest.mark.weird_functionality
-def test_add_weirder_stuff():
-    # Arrange
+def test_2_strings_input_to_add():
     calculator = Calculator()
-    # Act
     with pytest.raises(CalculatorError) as contxt:
         result = calculator.add("three", "two")
 
 
 @pytest.mark.basic_functionality
-def test_subtract():
-    # Arrange
+@pytest.mark.parametrize("input1,input2,expected", [(3, 1, 2),      # + + substraction
+                                                    (-3, 1, -4),    # - + substraction
+                                                    (3, -1, 4),    # + - substraction
+                                                    (-3, -1, -2)])  # - - substraction
+def test_subtract(input1, input2, expected):
     calculator = Calculator()
-    # Act
-    result = calculator.subtract(3, 1)
-    # Assert
-    assert result == 2, "test_add_assertion_error"
+    result = calculator.subtract(input1, input2)
+    assert result == expected, "test_add_assertion_error"
 
 
 @pytest.mark.basic_functionality
-def test_multiply():
-    # Arrange
+@pytest.mark.parametrize("input1,input2,expected", [(3, 1, 3),      # + + substraction
+                                                    (-3, 1, -3),    # - + substraction
+                                                    (3, -1, -3),    # + - substraction
+                                                    (-3, -1, 3)])  # - - substraction
+def test_multiply(input1, input2, expected):
     calculator = Calculator()
-    # Act
-    result = calculator.multiply(3, 2)
-    # Assert
-    assert result == 6, "test_add_assertion_error"
+    result = calculator.multiply(input1, input2)
+    assert result == expected, "test_add_assertion_error"
 
 
 @pytest.mark.basic_functionality
-def test_devide():
-    # Arrange
+@pytest.mark.parametrize("input1,input2,expected", [(3, 1, 3),      # + + substraction
+                                                    (-3, 1, -3),    # - + substraction
+                                                    (3, -1, -3),    # + - substraction
+                                                    (-3, -1, 3)])   # - - substraction
+def test_devide(input1, input2, expected):
     calculator = Calculator()
-    # Act
-    result = calculator.divide(3, 2)
-    # Assert
-    assert result == 1.5, "test_add_assertion_error"
+    result = calculator.divide(input1, input2)
+    assert result == expected, "test_add_assertion_error"
 
 
 @pytest.mark.weird_functionality
-def test_divide_by_zero():
+@pytest.mark.parametrize("input1,error_type", [(0.5, CalculatorError),     # fraction division
+                                               (5, CalculatorError),   # normal division
+                                               (0, CalculatorError)])  # zero by zero division
+def test_divide_by_zero(input1, error_type):
     calculator = Calculator()
-    with pytest.raises(CalculatorError):
-        result = calculator.divide(3, 0)
+    with pytest.raises(error_type):
+        result = calculator.divide(input1, 0)
